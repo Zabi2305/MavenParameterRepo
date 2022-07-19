18bd649e75a01ed38818ae129d04704365fb1394
hi <hi@Sangamesh123>
2022-07-19 15:33:32 +0530

    edited

--- MavenSelenium/src/test/java/com/tyss/sangamesh/datadriventesting/TestngTest.java
+++ MavenSelenium/src/test/java/com/tyss/sangamesh/datadriventesting/TestngTest.java
@@ -8,13 +8,12 @@ public class TestngTest {
 	public void testCase1Test() {
 		String browser = System.getProperty("browser");
 		String userName = System.getProperty("username");
-		 String password = System.getProperty("password");
-		
+		 
 		
 		
 		
 		System.out.println(userName);
-		System.out.println(password);
+		
 		
 	}
 	
