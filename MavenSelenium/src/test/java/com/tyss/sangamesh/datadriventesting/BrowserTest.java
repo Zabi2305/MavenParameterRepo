074756d9dc1cdd1fab37d7079ea58b65a8b8bfd3
hi <hi@Sangamesh123>
2022-07-20 16:05:12 +0530

    browser

--- MavenSelenium/src/test/java/com/tyss/sangamesh/datadriventesting/BrowserTest.java
+++ MavenSelenium/src/test/java/com/tyss/sangamesh/datadriventesting/BrowserTest.java
@@ -8,7 +8,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BrowserTest {
 	
 	@Test
-	public void browser() {
+	public void browserTest() {
 		
 		WebDriver driver=null;
 		
@@ -32,6 +32,34 @@ public class BrowserTest {
 		}
 		
 		System.out.println("hiiiiiiii");
+		
+	}
+	
+	@Test
+        public void browser1Test() {
+		
+		WebDriver driver=null;
+		
+		String URL = System.getProperty("url");
+	    String BROWSER = System.getProperty("browser");
+	    
+		
+		//Execute chrome browser
+		if (BROWSER.equalsIgnoreCase("chrome")) {
+			driver=WebDriverManager.chromedriver().create();
+		}
+		
+		//Execute firefox browser
+		else if (BROWSER.equalsIgnoreCase("firefox")) {
+			driver=WebDriverManager.firefoxdriver().create();
+		}
+		
+		//Execute MSEdge browser
+		else if (BROWSER.equalsIgnoreCase("edge")) {
+			driver=WebDriverManager.edgedriver().create();
+		}
+		
+		System.out.println("hellooooo!!!");
 		
 	}
 	
